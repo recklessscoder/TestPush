ee4c0ead63d9ee81f6140e48f8661e0fd20927cf
recklessscoder <57924564+recklessscoder@users.noreply.github.com>
2019-12-26 17:05:37 -0600

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("My first try");
         System.out.println("cybertek");
+        System.out.println("777cybertek");
     }
 }
